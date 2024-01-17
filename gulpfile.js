a60586eb5225f1aa97b554ef5baa305f5c03e15e
sdbty <asadbeeksafaraliyev@gmail.com>
2024-01-17 15:11:26 +0500

    rm

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+import gulp from "gulp";
+import autoPrefixer from "gulp-autoprefixer";
+import gcmq from "gulp-group-css-media-queries";
+import csso from "gulp-csso";
+import rename from "gulp-rename";
+import less from "gulp-less";
+import sourcemaps from "gulp-sourcemaps";
+
+gulp.task("la", pa);
+function pa() {
+    gulp.src("./from")
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(autoPrefixer({
+            cascade: true,
+            browsers: ["last 10 versions"]
+        }))
+        .pipe(gcmq())
+        .pipe(gulp.dest("./dest"))
+        .pipe(csso())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./dest"))
+}
+
+
+gulp.task("default" , gulp.parallel("la"))
+
+
+
+
+
+
